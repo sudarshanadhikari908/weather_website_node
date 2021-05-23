4c45fa96dd8c3033ca6d4b83eecf60821c9e2226
sudan <amstersudhan633@gmail.com>
2021-05-23 21:31:37 +0545

    version 1

---
+++ src/utils/forecast.js
@@ -0,0 +1,32 @@
+const request = require('request')
+const forecast = (lati, lang, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=9bd84da3f0ea93ddcfc0ac4467f14a82&query=' + encodeURIComponent(lang) + ',' + encodeURIComponent(lati) + '&units=f'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to network', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            callback(undefined, "The temperature is" + body.current.temperature + "It feels like" + body.current.feelslike
+
+                + "The summary is" + body.current.weather_descriptions[0]
+
+
+            )
+        }
+    })
+
+    //     }
+    //     else if (response.body.error) {
+    //         console.log('Unable to find Location')
+
+    //     } else {
+    //         console.log("it is currently" + response.body.current.temperature + "feels like" + response.body.current.feelslike + "% chance of rain")
+    //         console.log(response.body.current.weather_descriptions[0])
+    //     }
+    // })
+
+}
+module.exports = forecast
